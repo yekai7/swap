b5c3149891096663ba2daa54a8446471a0a3728c
yekai7 <yekai7@hotmail.com>
2019-12-13 22:29:25 +0800

    added env viables

--- server/readSetDbConfig.js
+++ server/readSetDbConfig.js
@@ -1,13 +1,26 @@
 const fs = require('fs')
 const dbConfig = () => {
     let dbConfig;
-    if (fs.existsSync(__dirname, './dbConfig.js')) {
-        dbConfig = require('./dbConfig');
-    } else {
+    try {
+        if (fs.existsSync(__dirname, './dbConfig.js')) {
+            dbConfig = require('./dbConfig');
+        }
+    } catch (e) {
         console.log("it went here")
         dbConfig = {
             mongo: {
                 url: process.env.MONGOURL
+            },
+            s3: {
+                accessKey: process.env.S3PUBLIC,
+                secret: process.env.S3SECRET,
+                endpoint_url: process.env.S3ENDPOINT
+            },
+            mysql: {
+                host: process.env.SQLHOST, port: 25060,
+                user: process.env.SQLUSER, password: process.env.SQLPWD,
+                database: 'swapit', connectionLimit: 4,
+                cacert: process.env.SQLCERT
             },
             tokenSecret: process.env.TOKENSECRET
         }
