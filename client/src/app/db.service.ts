841d92977b2e4241c5d693360756592fcc14c2c0
yekai7 <yekai7@hotmail.com>
2019-12-15 17:42:55 +0800

    test ssl

--- client/src/app/db.service.ts
+++ client/src/app/db.service.ts
@@ -20,8 +20,9 @@ export class DBService {
     return (this.router.parseUrl('/'));
   }
 
-  private url = 'http://localhost:3000'
+  // private url = 'http://localhost:3000'
   // private url = 'http://206.189.84.235:3000'
+  private url = 'https://206.189.84.235'
   // private url = 'https://swapit-server.herokuapp.com';
 
   loginUser(form): Promise<any> {
